836d58b1295933bac7a76c714a25f8e69634d643
Ethan Sewall, the Crab Keeper <s209066@students.aie.edu>
2020-10-29 16:55:18 -0700

    crab projectile generation

--- raygamecsharp/Program.cs
+++ raygamecsharp/Program.cs
@@ -27,6 +27,9 @@ namespace Crabination
             SpriteObject smallSprite = new SpriteObject();
             SceneObject collisionCircle = new SceneObject();
 
+            List<SceneObject> projectileCrabs = new List<SceneObject>();
+            List<SpriteObject> projectileSprites = new List<SpriteObject>();
+
             {
                 bigSprite.Load("crab.png");
                 smallSprite.Load("smallcrab.png");
@@ -38,9 +41,10 @@ namespace Crabination
 
                 bigSprite.SetRotate(-90 * (float)(Math.PI / 180.0f));
                 smallSprite.SetRotate(-90 * (float)(Math.PI / 180.0f));
-
+                bigSprite.Translate(-bigSprite.Height * 0.5f, bigSprite.Width * 0.5f);
                 bigCrab.SetPosition(800, 450);
-                smallCrab.SetPosition(bigSprite.Height * 0.65f, -bigSprite.Width * 0.25f);
+                smallSprite.Translate(-smallSprite.Height * 0.5f, smallSprite.Width * 0.5f);
+                smallCrab.Translate(smallSprite.Height * 0.5f, 0);
 
                 bigCrab.Rotate((float)(Math.PI * 0.5f));
             }//crab initialization stuff
@@ -55,55 +59,77 @@ namespace Crabination
 
                 Vector2 accelDirection = new Vector2();
 
-                if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_W) && bigCrab.IsInBounds())
                 {
-                    Vector3 facing = new Vector3( bigCrab.LocalTransform.m2, bigCrab.LocalTransform.m1, 1) * game.deltaTime * -150 * accel; 
-                    accelDirection += new Vector2(facing.y, -facing.x);
-                }
-                if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_A))
+                    if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_W) && bigCrab.IsInBounds())
+                    {
+                        Vector3 facing = new Vector3(bigCrab.LocalTransform.m2, bigCrab.LocalTransform.m1, 1) * game.deltaTime * -150 * accel;
+                        accelDirection += new Vector2(facing.y, -facing.x);
+                    }
+                    if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_A))
+                    {
+                        bigCrab.Rotate(-game.deltaTime * 2);
+                    }
+                    if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_S) && bigCrab.IsInBounds())
+                    {
+                        Vector3 facing = new Vector3(bigCrab.LocalTransform.m2, bigCrab.LocalTransform.m1, 1) * game.deltaTime * 150 * accel;
+                        accelDirection += new Vector2(facing.y, -facing.x);
+                    }
+                    if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_D))
+                    {
+                        bigCrab.Rotate(game.deltaTime * 2);
+                    }
+                    if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_Q))
+                    {
+                        smallCrab.Rotate(-game.deltaTime * 2);
+                    }
+                    if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_E))
+                    {
+                        smallCrab.Rotate(game.deltaTime * 2);
+                    }
+                    bigCrab.acceleration = accelDirection;
+                    bigCrab.velocity += bigCrab.acceleration;
+                    bigCrab.Translate(bigCrab.velocity.X, bigCrab.velocity.Y);
+                    bigCrab.velocity = new Vector2(bigCrab.velocity.X * drag, bigCrab.velocity.Y * drag);
+                    if (bigCrab.GlobalTransform.m6 < 0)
+                    {
+                        bigCrab.velocity.Y = MathF.Abs(bigCrab.velocity.Y);
+                    }
+                    if (bigCrab.GlobalTransform.m6 > 900)
+                    {
+                        bigCrab.velocity.Y = MathF.Abs(bigCrab.velocity.Y);
+                        bigCrab.velocity.Y = -bigCrab.velocity.Y;
+                    }
+                    if (bigCrab.GlobalTransform.m3 > 1600)
+                    {
+                        bigCrab.velocity.X = MathF.Abs(bigCrab.velocity.X);
+                        bigCrab.velocity.X = -bigCrab.velocity.X;
+                    }
+                    if (bigCrab.GlobalTransform.m3 < 0)
+                    {
+                        bigCrab.velocity.X = MathF.Abs(bigCrab.velocity.X);
+                    }
+                }//movement stuff
+
+                foreach(SceneObject crab in projectileCrabs)
                 {
-                    bigCrab.Rotate(-game.deltaTime);
-                }
-                if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_S) && bigCrab.IsInBounds())
-                {
-                    Vector3 facing = new Vector3(bigCrab.LocalTransform.m2, bigCrab.LocalTransform.m1, 1) * game.deltaTime * 150 * accel;
-                    accelDirection += new Vector2(facing.y, -facing.x);
-                }
-                if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_D))
-                {
-                    bigCrab.Rotate(game.deltaTime);
-                }
-                if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_Q))
-                {
-                    smallCrab.Rotate(-game.deltaTime);
-                }
-                if (IsKeyDown(Raylib_cs.KeyboardKey.KEY_E))
-                {
-                    smallCrab.Rotate(game.deltaTime);
-                }
-                bigCrab.acceleration = accelDirection;
-                bigCrab.velocity += bigCrab.acceleration;
-                bigCrab.Translate(bigCrab.velocity.X, bigCrab.velocity.Y);
-                bigCrab.velocity = new Vector2(bigCrab.velocity.X * drag, bigCrab.velocity.Y * drag);
-                if (bigCrab.GlobalTransform.m6 < 0)
-                {
-                    bigCrab.velocity.Y = MathF.Abs(bigCrab.velocity.Y);                   
-                }
-                if (bigCrab.GlobalTransform.m6 > 900)
-                {
-                    bigCrab.velocity.Y = MathF.Abs(bigCrab.velocity.Y);
-                    bigCrab.velocity.Y = -bigCrab.velocity.Y;
-                }
-                if (bigCrab.GlobalTransform.m3 > 1600)
-                {
-                    bigCrab.velocity.X = MathF.Abs(bigCrab.velocity.X);
-                    bigCrab.velocity.X = -bigCrab.velocity.X;
-                }
-                if (bigCrab.GlobalTransform.m3 < 0)
-                {
-                    bigCrab.velocity.X = MathF.Abs(bigCrab.velocity.X);
+                   // if(!crab.IsInBounds())
+                   // {
+                   //     projectileCrabs.Remove(crab);
+                   // }
+                    crab.Draw();
                 }
 
+                if(IsKeyDown(Raylib_cs.KeyboardKey.KEY_ENTER))
+                {
+                    SceneObject newProjectile = new SceneObject();
+                    SpriteObject newSprite = new SpriteObject();
+                    newSprite.Load("weapon_crab.png");
+                    newProjectile.AddChild(newSprite);
+                    newProjectile.SetPosition(bigCrab.GlobalTransform.m3, bigCrab.GlobalTransform.m6);
+
+                    projectileCrabs.Add(newProjectile);
+                    projectileSprites.Add(newSprite);
+                }
 
                 DrawCircle((int)collisionCircle.GlobalTransform.m3, (int)collisionCircle.GlobalTransform.m6, 60, WHITE);
 
@@ -115,7 +141,7 @@ namespace Crabination
 
             CloseWindow();
 
-            return 0;
+            return 1;
         }
     }
 
